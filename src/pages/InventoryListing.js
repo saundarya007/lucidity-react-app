7fdd59d489701295bbfcdc588054d58e632d6d0f
Saundarya Saundarya <saundarya.saundarya@gdn-commerce.com>
2024-04-09 12:23:00 +0530

    code clean

--- src/pages/InventoryListing.js
+++ src/pages/InventoryListing.js
@@ -3,43 +3,6 @@ import InfoWidges from "../components/InfoWidges";
 import ListingTable from "../components/ListingTable";
 
 function InventoryListing() {
-  const dummyResponse = [
-    {
-    "name": "Bluetooth",
-    "category": "Electronic",
-    "value": "$150",
-    "quantity": 5,
-    "price": "$30"
-    },
-    {
-    "name": "Edifier M43560",
-    "category": "Electronic",
-    "value": "0",
-    "quantity": 0,
-    "price": "$0"
-    },
-    {
-    "name": "Sony 4k ultra 55 inch TV",
-    "category": "Electronic",
-    "value": "$1190",
-    "quantity": 17,
-    "price": "$70"
-    },
-    {
-    "name": "Samsumg 55 inch TV",
-    "category": "Electronic",
-    "value": "$600",
-    "quantity": 50,
-    "price": "$12"
-    },
-    {
-    "name": "samsumg S34 Ultra",
-    "category": "phone",
-    "value": "0",
-    "quantity": 0,
-    "price": "$0"
-    }
-  ]
   const [inventoryList, setInventoryList] = useState([]);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
@@ -76,31 +39,47 @@ function InventoryListing() {
   };
 
   useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
     const fetchData = async () => {
       try {
-        const response = await fetch('https://9c7becb8dfe2420e9e7e5f3ff4352700.api.mockbin.io/');
+        const response = await fetch('https://dev-0tf0hinghgjl39z.api.raw-labs.com/inventory', { signal });
+        
         if (!response.ok) {
           throw new Error('Failed to fetch data');
         }
-        const jsonData = dummyResponse;
-        setInventoryList(jsonData);
-        if (jsonData.length) {
-          const uniqueCategories = new Set(jsonData.map(item => item.category));
-          setWidgesCount({
-            totalProduct: jsonData.length,
-            totalStoreValue: jsonData.reduce((total, item) => total + parseFloat(item.value.replace('$', '')) * item.quantity, 0),
-            outOfStock: jsonData.filter(item => item.quantity === 0).length,
-            noOfCategory: uniqueCategories.size
-          });
+
+        const jsonData = await response.json();
+
+        if (!signal.aborted) { // Check if the component is still mounted
+          setInventoryList(jsonData);
+
+          if (jsonData.length) {
+            const uniqueCategories = new Set(jsonData.map(item => item.category));
+            setWidgesCount({
+              totalProduct: jsonData.length,
+              totalStoreValue: jsonData.reduce((total, item) => total + parseFloat(item.value.replace('$', '')) * item.quantity, 0),
+              outOfStock: jsonData.filter(item => item.quantity === 0).length,
+              noOfCategory: uniqueCategories.size
+            });
+          }
+          setIsLoading(false);
         }
-        setIsLoading(false);
       } catch (error) {
-        setError(error);
-        setIsLoading(false);
+        if (!signal.aborted) { // Check if the component is still mounted
+          setError(error);
+          setIsLoading(false);
+        }
       }
     };
 
     fetchData();
+
+    // Function to cancel the API call if the component unmounts
+    return () => {
+      controller.abort();
+    };
   }, []);
 
   if (isLoading) {
